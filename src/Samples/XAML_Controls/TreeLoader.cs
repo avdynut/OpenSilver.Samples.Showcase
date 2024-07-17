a003476b10d1001dc0a4de000d09a5fba7de1e24
Andrei Arekhva <a.arekhva@gmail.com>
2024-07-17 18:01:11 +0300

    Detect when the visual tree is fully loaded

---
+++ src/Samples/XAML_Controls/TreeLoader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace OpenSilver.Samples.Showcase
+{
+    public class TreeLoader
+    {
+        private int _timersCount;
+        private int _fullyLoadedElementsCount;
+
+        public event EventHandler TreeFullyLoaded;
+
+        public void Initialize(FrameworkElement rootElement, TimeSpan loadDelay)
+        {
+            SubscribeToLayoutUpdated(rootElement, loadDelay);
+        }
+
+        private void SubscribeToLayoutUpdated(FrameworkElement element, TimeSpan loadDelay)
+        {
+            var timer = new DispatcherTimer { Interval = loadDelay };
+
+            timer.Tick += (s, e) =>
+            {
+                // If no layout updates have occurred within the interval, the element is considered fully loaded
+                element.LayoutUpdated -= OnElementLayoutUpdated1;
+                timer.Stop();
+                _fullyLoadedElementsCount++;
+
+                // Traverse visual children
+                int childCount = VisualTreeHelper.GetChildrenCount(element);
+                for (int i = 0; i < childCount; i++)
+                {
+                    var child = VisualTreeHelper.GetChild(element, i);
+                    if (child is FrameworkElement frameworkElement)
+                    {
+                        SubscribeToLayoutUpdated(frameworkElement, loadDelay);
+                    }
+                }
+
+                if (_fullyLoadedElementsCount == _timersCount)
+                {
+                    OnTreeFullyLoaded();
+                }
+            };
+
+            _timersCount++;
+
+            element.LayoutUpdated += OnElementLayoutUpdated1;
+            timer.Start();
+
+            void OnElementLayoutUpdated1(object sender, EventArgs e)
+            {
+                // Reset the timer whenever a layout update occurs
+                timer.Stop();
+                timer.Start();
+            }
+        }
+
+        private void OnTreeFullyLoaded()
+        {
+            TreeFullyLoaded?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void TraverseElements(DependencyObject element, Action<DependencyObject> action)
+        {
+            action(element);
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(element);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i);
+                TraverseElements(child, action);
+            }
+        }
+    }
+}
--- src/Samples/XAML_Controls/Xaml_Controls.xaml.cs
+++ src/Samples/XAML_Controls/Xaml_Controls.xaml.cs
@@ -1,10 +1,17 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace OpenSilver.Samples.Showcase
 {
     public partial class Xaml_Controls : UserControl
     {
+        private readonly Stopwatch _stopwatch = Stopwatch.StartNew();
+        private readonly TreeLoader _treeLoader;
+
         public Xaml_Controls()
         {
             this.InitializeComponent();
@@ -15,6 +22,59 @@ namespace OpenSilver.Samples.Showcase
             ScrollBarDemo.Visibility = Visibility.Collapsed;
             ThumbDemo.Visibility = Visibility.Collapsed;
             FrameDemo.Visibility = Visibility.Collapsed; // The Showcase already uses a Frame to change pages anyway
+
+            PrintLog("Xaml_Controls()");
+            Loaded += OnXaml_Controls_Loaded;
+
+            _treeLoader = new TreeLoader();
+            _treeLoader.TreeFullyLoaded += TreeLoader_TreeFullyLoaded;
+            _treeLoader.Initialize(Application.Current.MainWindow, TimeSpan.FromMilliseconds(10));
+        }
+
+        private async void OnXaml_Controls_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(5000);
+            ApplyTreeElementsPostProcessing(Application.Current.MainWindow);
+        }
+
+        private void ApplyTreeElementsPostProcessing(DependencyObject element)
+        {
+            var elementsCount = 0;
+            TraverseNextElement(element);
+            PrintLog($"Elements: {elementsCount}");
+
+            void TraverseNextElement(DependencyObject el)
+            {
+                elementsCount++;
+                //Some additional post processing.
+                //For example, we restrict popups to the view port area.
+                //if (el is Popup popup)
+                //{
+                //    PopupManager.RestrictPopup(popup);
+                //}
+
+                var count = VisualTreeHelper.GetChildrenCount(el);
+                for (var i = 0; i < count; i++)
+                {
+                    var child = VisualTreeHelper.GetChild(el, i);
+                    TraverseNextElement(child);
+                }
+            }
+        }
+
+        private void TreeLoader_TreeFullyLoaded(object sender, EventArgs e)
+        {
+            var count = 0;
+            _treeLoader.TraverseElements(Application.Current.MainWindow, element =>
+            {
+                count++;
+            });
+            PrintLog($"TreeLoader_TreeFullyLoaded {count}");
+        }
+
+        private void PrintLog(string message)
+        {
+            Console.WriteLine($"{_stopwatch.ElapsedMilliseconds} ms: {message}");
         }
     }
 }
